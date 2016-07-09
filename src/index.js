25cc72522f60dba530812d83df32e153d5dd737d
mottox2 <y.mottox2@gmail.com>
2016-07-10 01:45:53 +0900

    Add handleKeyCommand

--- src/index.js
+++ src/index.js
@@ -1,7 +1,7 @@
 import React from 'react'
 import { render } from 'react-dom'
 
-import {Editor, EditorState} from 'draft-js';
+import {Editor, EditorState, RichUtils} from 'draft-js';
 
 export default class App extends React.Component {
   constructor(props) {
@@ -16,12 +16,22 @@ export default class App extends React.Component {
     this.setState({editorState})
   }
 
+  handleKeyCommand(command) {
+    const newState = RichUtils.handleKeyCommand(this.state.editorState, command)
+    if (newState) {
+      this.onChange(newState)
+      return true
+    }
+    return false
+  }
+
   render() {
     return <div>
       <h1>Draft.js example</h1>
       <Editor
         editorState={this.state.editorState}
         onChange={this.onChange.bind(this)}
+        handleKeyCommand={this.handleKeyCommand.bind(this)}
       />
     </div>
   }
